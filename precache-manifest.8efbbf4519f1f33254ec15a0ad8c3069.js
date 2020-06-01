f339b9e3aa0f3e26dd26cdd45c2ae9485af88ed5
Lonewolf46 <rjpruthvi@gmail.com>
2020-06-01 11:25:09 +0530

    Updates

---
+++ precache-manifest.8efbbf4519f1f33254ec15a0ad8c3069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4102e5ffcbade04e7c0e654d9234b168",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "be68c784d05904da5d90",
+    "url": "/myportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "cf32befa3c72c5d8ac1c",
+    "url": "/myportfolio/static/css/main.2a8874e3.chunk.css"
+  },
+  {
+    "revision": "be68c784d05904da5d90",
+    "url": "/myportfolio/static/js/2.69becae7.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myportfolio/static/js/2.69becae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf32befa3c72c5d8ac1c",
+    "url": "/myportfolio/static/js/main.aa080810.chunk.js"
+  },
+  {
+    "revision": "dfcaa98cefeb0fdc319c",
+    "url": "/myportfolio/static/js/runtime-main.6ecf7dfe.js"
+  }
+]);
